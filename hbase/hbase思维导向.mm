dd30a0f194a44135f1c0f9a6a10f43047039d398
liaohualue <m18775128300@163.com>
2018-03-06 10:02:28 +0800

    hbase分布式实时数据库

---
+++ hbase/hbase思维导向.mm
@@ -0,0 +1,196 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1519961460719" ID="ID_318282628" MODIFIED="1520214978068" TEXT="hbase&#x601d;&#x7ef4;&#x5bfc;&#x5411;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519961676196" ID="ID_371523863" MODIFIED="1520215064762" POSITION="right" TEXT="hadood&#x7684;&#x751f;&#x6001;&#x7cfb;&#x7edf;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519961689601" ID="ID_1032472602" MODIFIED="1520214984212">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="hadoop&#x751f;&#x6001;&#x7cfb;&#x7edf;.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519961726288" ID="ID_1540902501" MODIFIED="1520214984212" POSITION="right" TEXT="&#x7b80;&#x4ecb;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519961751885" ID="ID_1468372285" MODIFIED="1520214984212" TEXT="1.Hadoop hbase&#x662f;&#x4e00;&#x4e2a;&#x9ad8;&#x53ef;&#x9760;&#xff0c;&#x9ad8;&#x6027;&#x80fd;&#xff0c;&#x9762;&#x5411;&#x5217;&#xff0c;&#x53ef;&#x4f38;&#x7f29;&#x5b9e;&#x65f6;&#x8bfb;&#x5199;&#x7684;&#x5206;&#x5e03;&#x5f0f;&#x6570;&#x636e;&#x5e93;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1519961845077" ID="ID_738201453" MODIFIED="1520214984212" TEXT="2. Hadoop HDFS&#x4f5c;&#x4e3a;&#x5176;&#x6587;&#x4ef6;&#x5b58;&#x50a8;&#x7cfb;&#x7edf;&#xff0c;&#x5229;&#x7528;MR&#x6765;&#x5904;&#x7406;Hbase&#x4e2d;&#x7684;&#x6d77;&#x91cf;&#x6570;&#x636e;&#xff0c;&#x5229;&#x7528;Zookeeper&#x4e3a;&#x5176;&#x5206;&#x5e03;&#x5f0f;&#x534f;&#x540c;&#x5de5;&#x5177;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1519961965511" ID="ID_529568917" MODIFIED="1520214984196" TEXT="3&#xff0c;&#x4e3b;&#x8981;&#x7528;&#x6765;&#x5b58;&#x50a8;&#x975e;&#x7ed3;&#x6784;&#x5316;&#x548c;&#x534a;&#x7ed3;&#x6784;&#x5316;&#x7684;&#x677e;&#x6563;&#x6570;&#x636e;&#xff08;NoSql&#x6570;&#x636e;&#xff09;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519962828518" ID="ID_775340966" MODIFIED="1520214984196" POSITION="right" TEXT="&#x6570;&#x636e;&#x6a21;&#x578b;&#x56fe;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519962839759" ID="ID_1715059934" MODIFIED="1520214984181">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="hbase&#x6570;&#x636e;&#x6a21;&#x578b;&#x56fe;.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519962024142" ID="ID_611510571" MODIFIED="1520214984181" POSITION="right" TEXT="Hbase&#x6570;&#x636e;&#x6a21;&#x578b;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519962040063" ID="ID_573225683" MODIFIED="1520214984181" TEXT="1 ROWKEY">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519962123031" ID="ID_1829088966" MODIFIED="1520214984181" TEXT="&#x51b3;&#x5b9a;&#x4e00;&#x884c;&#x6570;&#x636e;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1519962134888" ID="ID_1833197984" MODIFIED="1520214984181" TEXT="&#x6309;&#x7167;&#x5b57;&#x5178;&#x5e8f;&#x987a;&#x5e8f;&#x6392;&#x5217;&#xff08;&#x5982; a b c d e f g&#xff09;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1519962162861" ID="ID_374841615" MODIFIED="1520214984181" TEXT="Row key&#x53ea;&#x80fd;&#x5b58;&#x50a8;64&#x7684;&#x5b57;&#x8282;&#x6570;&#x636e;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519962205301" ID="ID_1518208453" MODIFIED="1520214984181" TEXT="2 column Family&#x5217;&#x65cf;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519962258211" ID="ID_148162094" MODIFIED="1520214984181" TEXT="&#x6bcf;&#x4e2a;&#x5217;&#x90fd;&#x5f52;&#x5c5e;&#x4e8e;&#x67d0;&#x4e2a;&#x5217;&#x65cf;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1519962307573" ID="ID_867082482" MODIFIED="1520214984165" TEXT="&#x5217;&#x65cf;&#x5fc5;&#x987b;&#x4f5c;&#x4e3a;&#x8868;&#x6a21;&#x5f0f;&#xff08;schema&#xff09;&#x5b9a;&#x4e49;&#x7684;&#x4e00;&#x90e8;&#x5206;&#x9884;&#x5148;&#x7ed9;&#x51fa;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519962367553" ID="ID_39198830" MODIFIED="1520214984165" TEXT="&#x5982; create &#x2018;test&#x2019;, &#x2018;course&#x2019;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+<node CREATED="1519962396460" ID="ID_177406631" MODIFIED="1520214984165" TEXT="3 &#x5217;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519962408635" ID="ID_207873508" MODIFIED="1520214984165" TEXT="&#x5217;&#x540d;&#x4ee5;&#x5217;&#x65cf;&#x4f5c;&#x4e3a;&#x524d;&#x7f00;&#xff0c;&#x6bcf;&#x4e2a;&#x5217;&#x65cf;&#x90fd;&#x53ef;&#x4ee5;&#x6709;&#x591a;&#x4e2a;&#x5217;&#x6210;&#x5458;&#xff08;column&#xff09;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519962470615" ID="ID_235200373" MODIFIED="1520214984165" TEXT="&#x5982; course:math, course:english, &#x65b0;&#x7684;&#x5217;&#x65cf;&#x6210;&#x5458;&#xff08;&#x5217;&#xff09;&#x53ef;&#x4ee5;&#x968f;&#x540e;&#x6309;&#x9700;&#x3001;&#x52a8;&#x6001;&#x52a0; &#x5165;&#xff1b;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+<node CREATED="1519977633136" ID="ID_1748668679" MODIFIED="1520214984165" TEXT="4 &#x65f6;&#x95f4;&#x6233;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519977648805" HGAP="23" ID="ID_386893535" MODIFIED="1520214984149" TEXT="&#x5728;HBase&#x6bcf;&#x4e2a;cell&#x5b58;&#x50a8;&#x5355;&#x5143;&#x5bf9;&#x540c;&#x4e00;&#x4efd;&#x6570;&#x636e;&#x6709;&#x591a;&#x4e2a;&#x7248;&#x672c;&#xff0c;&#x6839;&#x636e;&#x552f;&#x4e00;&#x7684;&#x65f6;&#x95f4; &#x6233;&#x6765;&#x533a;&#x5206;&#x6bcf;&#x4e2a;&#x7248;&#x672c;&#x4e4b;&#x95f4;&#x7684;&#x5dee;&#x5f02;&#xff0c;&#x4e0d;&#x540c;&#x7248;&#x672c;&#x7684;&#x6570;&#x636e;&#x6309;&#x7167;&#x65f6;&#x95f4;&#x5012;&#x5e8f;&#x6392;&#x5e8f;&#xff0c; &#x6700;&#x65b0;&#x7684;&#x6570;&#x636e;&#x7248;&#x672c;&#x6392;&#x5728;&#x6700;&#x524d;&#x9762;&#x3002; &#x2013; &#x65f6;&#x95f4;&#x6233;&#x7684;&#x7c7b;&#x578b;&#x662f; 64&#x4f4d;&#x6574;&#x578b;&#x3002; &#x2013; &#x65f6;&#x95f4;&#x6233;&#x53ef;&#x4ee5;&#x7531;HBase(&#x5728;&#x6570;&#x636e;&#x5199;&#x5165;&#x65f6;&#x81ea;&#x52a8;)&#x8d4b;&#x503c;&#xff0c;&#x6b64;&#x65f6;&#x65f6;&#x95f4;&#x6233;&#x662f;&#x7cbe;&#x786e;&#x5230;&#x6beb; &#x79d2;&#x7684;&#x5f53;&#x524d;&#x7cfb;&#x7edf;&#x65f6;&#x95f4;&#x3002; &#x2013; &#x65f6;&#x95f4;&#x6233;&#x4e5f;&#x53ef;&#x4ee5;&#x7531;&#x5ba2;&#x6237;&#x663e;&#x5f0f;&#x8d4b;&#x503c;&#xff0c;&#x5982;&#x679c;&#x5e94;&#x7528;&#x7a0b;&#x5e8f;&#x8981;&#x907f;&#x514d;&#x6570;&#x636e;&#x7248;&#x672c;&#x51b2;&#x7a81;&#xff0c; &#x5c31;&#x5fc5;&#x987b;&#x81ea;&#x5df1;&#x751f;&#x6210;&#x5177;&#x6709;&#x552f;&#x4e00;&#x6027;&#x7684;&#x65f6;&#x95f4;&#x6233;&#x3002;" VSHIFT="2">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519977693532" ID="ID_455289078" MODIFIED="1520214984149" TEXT="5 Cell&#x5355;&#x5143;&#x683c;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519977699478" ID="ID_820918506" MODIFIED="1520214984134" TEXT="&#x7531;&#x884c;&#x548c;&#x5217;&#x7684;&#x5750;&#x6807;&#x4ea4;&#x53c9;&#x51b3;&#x5b9a;&#xff1b; &#x2013; &#x5355;&#x5143;&#x683c;&#x662f;&#x6709;&#x7248;&#x672c;&#x7684;&#xff1b; &#x2013; &#x5355;&#x5143;&#x683c;&#x7684;&#x5185;&#x5bb9;&#x662f;&#x672a;&#x89e3;&#x6790;&#x7684;&#x5b57;&#x8282;&#x6570;&#x7ec4;&#xff1b; &#x2022; &#x7531;{row key&#xff0c; column( =&lt;family&gt; +&lt;qualifier&gt;)&#xff0c; version} &#x552f;&#x4e00;&#x786e;&#x5b9a;&#x7684;&#x5355;&#x5143;&#x3002; cell&#x4e2d;&#x7684;&#x6570;&#x636e;&#x662f;&#x6ca1; &#x6709;&#x7c7b;&#x578b;&#x7684;&#xff0c;&#x5168;&#x90e8;&#x662f;&#x5b57;&#x8282;&#x7801;&#x5f62;&#x5f0f;&#x5b58;&#x8d2e;&#x3002;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519977793250" ID="ID_1999733972" MODIFIED="1520214984134" TEXT="6 HLOG">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519977804558" ID="ID_1941041208" MODIFIED="1520214984134" TEXT="HLog&#x6587;&#x4ef6;&#x5c31;&#x662f;&#x4e00;&#x4e2a;&#x666e;&#x901a;&#x7684;Hadoop Sequence File&#xff0c;Sequence File &#x7684;Key&#x662f; HLogKey&#x5bf9;&#x8c61;&#xff0c;HLogKey&#x4e2d;&#x8bb0;&#x5f55;&#x4e86;&#x5199;&#x5165;&#x6570;&#x636e;&#x7684;&#x5f52;&#x5c5e;&#x4fe1;&#x606f;&#xff0c;&#x9664;&#x4e86;table&#x548c; region&#x540d;&#x5b57;&#x5916;&#xff0c;&#x540c;&#x65f6;&#x8fd8;&#x5305;&#x62ec; sequence number&#x548c;timestamp&#xff0c;timestamp&#x662f;&#x201d; &#x5199;&#x5165;&#x65f6;&#x95f4;&#x201d;&#xff0c;sequence number&#x7684;&#x8d77;&#x59cb;&#x503c;&#x4e3a;0&#xff0c;&#x6216;&#x8005;&#x662f;&#x6700;&#x8fd1;&#x4e00;&#x6b21;&#x5b58;&#x5165;&#x6587;&#x4ef6;&#x7cfb; &#x7edf;&#x4e2d;sequence number&#x3002; &#x2013; HLog SequeceFile&#x7684;Value&#x662f;HBase&#x7684;KeyValue&#x5bf9;&#x8c61;&#xff0c;&#x5373;&#x5bf9;&#x5e94;HFile&#x4e2d;&#x7684; KeyValue">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519978149375" ID="ID_855236220" MODIFIED="1520214984134" TEXT="Memstore &#x4e0e; storefile">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978153917" ID="ID_1400230428" MODIFIED="1520214984118" TEXT="&#x4e00;&#x4e2a;region&#x7531;&#x591a;&#x4e2a;store&#x7ec4;&#x6210;&#xff0c;&#x4e00;&#x4e2a;store&#x5bf9;&#x5e94;&#x4e00;&#x4e2a;CF&#xff08;&#x5217;&#x65cf;&#xff09; &#x2013; store&#x5305;&#x62ec;&#x4f4d;&#x4e8e;&#x5185;&#x5b58;&#x4e2d;&#x7684;memstore&#x548c;&#x4f4d;&#x4e8e;&#x78c1;&#x76d8;&#x7684;storefile&#x5199;&#x64cd;&#x4f5c;&#x5148;&#x5199;&#x5165; memstore&#xff0c;&#x5f53;memstore&#x4e2d;&#x7684;&#x6570;&#x636e;&#x8fbe;&#x5230;&#x67d0;&#x4e2a;&#x9608;&#x503c;&#xff0c;hregionserver&#x4f1a;&#x542f;&#x52a8; flashcache&#x8fdb;&#x7a0b;&#x5199;&#x5165;storefile&#xff0c;&#x6bcf;&#x6b21;&#x5199;&#x5165;&#x5f62;&#x6210;&#x5355;&#x72ec;&#x7684;&#x4e00;&#x4e2a;storefile &#x2013; &#x5f53;storefile&#x6587;&#x4ef6;&#x7684;&#x6570;&#x91cf;&#x589e;&#x957f;&#x5230;&#x4e00;&#x5b9a;&#x9608;&#x503c;&#x540e;&#xff0c;&#x7cfb;&#x7edf;&#x4f1a;&#x8fdb;&#x884c;&#x5408;&#x5e76;&#xff08;minor&#x3001; major compaction&#xff09;&#xff0c;&#x5728;&#x5408;&#x5e76;&#x8fc7;&#x7a0b;&#x4e2d;&#x4f1a;&#x8fdb;&#x884c;&#x7248;&#x672c;&#x5408;&#x5e76;&#x548c;&#x5220;&#x9664;&#x5de5;&#x4f5c; &#xff08;majar&#xff09;&#xff0c;&#x5f62;&#x6210;&#x66f4;&#x5927;&#x7684;storefile &#x2013; &#x5f53;&#x4e00;&#x4e2a;region&#x6240;&#x6709;storefile&#x7684;&#x5927;&#x5c0f;&#x548c;&#x8d85;&#x8fc7;&#x4e00;&#x5b9a;&#x9608;&#x503c;&#x540e;&#xff0c;&#x4f1a;&#x628a;&#x5f53;&#x524d;&#x7684;region &#x5206;&#x5272;&#x4e3a;&#x4e24;&#x4e2a;&#xff0c;&#x5e76;&#x7531;hmaster&#x5206;&#x914d;&#x5230;&#x76f8;&#x5e94;&#x7684;regionserver&#x670d;&#x52a1;&#x5668;&#xff0c;&#x5b9e;&#x73b0;&#x8d1f;&#x8f7d; &#x5747;&#x8861; &#x2013; &#x5ba2;&#x6237;&#x7aef;&#x68c0;&#x7d22;&#x6570;&#x636e;&#xff0c;&#x5148;&#x5728;memstore&#x627e;&#xff0c;&#x627e;&#x4e0d;&#x5230;&#x518d;&#x627e;storefile">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519978221849" ID="ID_1882359704" MODIFIED="1520214984118" TEXT="HBase&#x6570;&#x636e;&#x6a21;&#x578b;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978253548" ID="ID_1905246766" MODIFIED="1520214984118" TEXT="&#x56fe;&#x4e00;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978352038" ID="ID_714158846" MODIFIED="1520214984087">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x6570;&#x636e;&#x6a21;&#x578b;1.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519978356452" ID="ID_1185005069" MODIFIED="1520214984071" TEXT="&#x56fe;&#x4e8c;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978364609" ID="ID_87825605" MODIFIED="1520214984024">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x6570;&#x636e;&#x6a21;&#x578b;2.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1519977840640" ID="ID_120130953" MODIFIED="1520214984024" POSITION="right" TEXT="HBase&#x4f53;&#x7cfb;&#x67b6;&#x6784;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519977845785" ID="ID_246438761" MODIFIED="1520214984024" TEXT="&#x4f53;&#x7cfb;&#x673a;&#x6784;&#x56fe;">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519977858707" ID="ID_1673722566" MODIFIED="1520214983977">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="HBase&#x4f53;&#x7cfb;&#x673a;&#x6784;&#x56fe;.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519977967734" ID="ID_490007489" MODIFIED="1520214983977" TEXT="Client">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519977972691" ID="ID_474191265" MODIFIED="1520214983977" TEXT="&#x5305;&#x542b;&#x8bbf;&#x95ee;HBase&#x7684;&#x63a5;&#x53e3;&#x5e76;&#x7ef4;&#x62a4;cache&#x6765;&#x52a0;&#x5feb;&#x5bf9;HBase&#x7684;&#x8bbf;&#x95ee;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519977991685" ID="ID_1479286100" MODIFIED="1520214983977" TEXT="Zookeeper">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978018720" ID="ID_984880338" MODIFIED="1520221840144" TEXT="&#x4fdd;&#x8bc1;&#x4efb;&#x4f55;&#x65f6;&#x5019;&#xff0c;&#x96c6;&#x7fa4;&#x4e2d;&#x53ea;&#x6709;&#x4e00;&#x4e2a;master &#x2022; &#x5b58;&#x8d2e;&#x6240;&#x6709;Region(&#x76f8;&#x5f53;&#x4e8e;&#x4e00;&#x4e2a;table&#x8868;)&#x7684;&#x5bfb;&#x5740;&#x5165;&#x53e3;&#x3002; &#x2022; &#x5b9e;&#x65f6;&#x76d1;&#x63a7;Region server&#xff08;&#x7c7b;&#x4f3c;hdfs&#x4e0a;&#x7684;dataNode&#x5b9e;&#x9645;&#x5e72;&#x6d3b;&#xff0c;&#x6253;&#x5de5;&#x4ed4;&#xff09;&#x7684;&#x4e0a;&#x7ebf;&#xff08;&#x6b63;&#x5e38;&#x542f;&#x52a8;&#xff09;&#x548c;&#x4e0b;&#x7ebf;&#xff08;&#x6b7b;&#x4e86;&#xff0c;&#x6302;&#x6389;&#x7684;&#x610f;&#x601d;&#xff09;&#x4fe1;&#x606f;&#x3002; &#x5e76;&#x5b9e;&#x65f6;&#x901a;&#x77e5;Master &#x2022; &#x5b58;&#x50a8;HBase&#x7684;schema&#x548c;table&#x5143;&#x6570;&#x636e;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519978057156" ID="ID_1656069774" MODIFIED="1520214983962" TEXT="Master">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978061504" ID="ID_328423772" MODIFIED="1520221879272" TEXT="&#x4e3a;Region server&#x5206;&#x914d;region&#xff08;&#x76f8;&#x5f53;&#x4e8e;table&#x8868;&#xff09; &#x2022; &#x8d1f;&#x8d23;Region server&#x7684;&#x8d1f;&#x8f7d;&#x5747;&#x8861; &#x2022; &#x53d1;&#x73b0;&#x5931;&#x6548;&#x7684;Region server&#x5e76;&#x91cd;&#x65b0;&#x5206;&#x914d;&#x5176;&#x4e0a;&#x7684;region &#x2022; &#x7ba1;&#x7406;&#x7528;&#x6237;&#x5bf9;table&#x7684;&#x589e;&#x5220;&#x6539;&#x64cd;&#x4f5c;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519978084736" ID="ID_962996321" MODIFIED="1520214983962" TEXT="RegionServer">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978089116" ID="ID_1991116142" MODIFIED="1520221971216" TEXT="Region server&#xff08;&#x7c7b;&#x4f3c;&#x4e8e;hdfs&#x4e0a;&#x7684;dataNode&#x5b9e;&#x9645;&#x5e72;&#x6d3b;&#x7684;&#xff0c;&#x6253;&#x5de5;&#x4ed4;&#xff09;&#x7ef4;&#x62a4;region&#xff0c;&#x5904;&#x7406;&#x5bf9;&#x8fd9;&#x4e9b;region&#x7684;IO&#x8bf7;&#x6c42; &#x2022; Region server&#x8d1f;&#x8d23;&#x5207;&#x5206;&#x5728;&#x8fd0;&#x884c;&#x8fc7;&#x7a0b;&#x4e2d;&#x53d8;&#x5f97;&#x8fc7;&#x5927;&#x7684;region">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519978116872" ID="ID_1061999629" MODIFIED="1520214983946" TEXT="Region">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978121050" ID="ID_1368140278" MODIFIED="1520214983931" TEXT="HBase&#x81ea;&#x52a8;&#x628a;&#x8868;&#x6c34;&#x5e73;&#x5212;&#x5206;&#x6210;&#x591a;&#x4e2a;&#x533a;&#x57df;(region)&#xff0c;&#x6bcf;&#x4e2a;region&#x4f1a;&#x4fdd;&#x5b58;&#x4e00;&#x4e2a;&#x8868; &#x91cc;&#x9762;&#x67d0;&#x6bb5;&#x8fde;&#x7eed;&#x7684;&#x6570;&#x636e;&#xff1b;&#x6bcf;&#x4e2a;&#x8868;&#x4e00;&#x5f00;&#x59cb;&#x53ea;&#x6709;&#x4e00;&#x4e2a;region&#xff0c;&#x968f;&#x7740;&#x6570;&#x636e;&#x4e0d;&#x65ad;&#x63d2; &#x5165;&#x8868;&#xff0c;region&#x4e0d;&#x65ad;&#x589e;&#x5927;&#xff0c;&#x5f53;&#x589e;&#x5927;&#x5230;&#x4e00;&#x4e2a;&#x9600;&#x503c;&#x7684;&#x65f6;&#x5019;&#xff0c;region&#x5c31;&#x4f1a;&#x7b49;&#x5206;&#x4f1a; &#x4e24;&#x4e2a;&#x65b0;&#x7684;region&#xff08;&#x88c2;&#x53d8;&#xff09;&#xff1b; &#x2013; &#x5f53;table&#x4e2d;&#x7684;&#x884c;&#x4e0d;&#x65ad;&#x589e;&#x591a;&#xff0c;&#x5c31;&#x4f1a;&#x6709;&#x8d8a;&#x6765;&#x8d8a;&#x591a;&#x7684;region&#x3002; &#x8fd9;&#x6837;&#x4e00;&#x5f20;&#x5b8c;&#x6574;&#x7684;&#x8868; &#x88ab;&#x4fdd;&#x5b58;&#x5728;&#x591a;&#x4e2a;Regionserver &#x4e0a;&#x3002;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1519978149375" ID="ID_1173012544" MODIFIED="1520214983931" TEXT="Memstore &#x4e0e; storefile">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1519978153917" ID="ID_675370594" MODIFIED="1520214983884" TEXT="&#x4e00;&#x4e2a;region&#x7531;&#x591a;&#x4e2a;store&#x7ec4;&#x6210;&#xff0c;&#x4e00;&#x4e2a;store&#x5bf9;&#x5e94;&#x4e00;&#x4e2a;CF&#xff08;&#x5217;&#x65cf;&#xff09; &#x2013; store&#x5305;&#x62ec;&#x4f4d;&#x4e8e;&#x5185;&#x5b58;&#x4e2d;&#x7684;memstore&#x548c;&#x4f4d;&#x4e8e;&#x78c1;&#x76d8;&#x7684;storefile&#x5199;&#x64cd;&#x4f5c;&#x5148;&#x5199;&#x5165; memstore&#xff0c;&#x5f53;memstore&#x4e2d;&#x7684;&#x6570;&#x636e;&#x8fbe;&#x5230;&#x67d0;&#x4e2a;&#x9608;&#x503c;&#xff0c;hregionserver&#x4f1a;&#x542f;&#x52a8; flashcache&#x8fdb;&#x7a0b;&#x5199;&#x5165;storefile&#xff0c;&#x6bcf;&#x6b21;&#x5199;&#x5165;&#x5f62;&#x6210;&#x5355;&#x72ec;&#x7684;&#x4e00;&#x4e2a;storefile &#x2013; &#x5f53;storefile&#x6587;&#x4ef6;&#x7684;&#x6570;&#x91cf;&#x589e;&#x957f;&#x5230;&#x4e00;&#x5b9a;&#x9608;&#x503c;&#x540e;&#xff0c;&#x7cfb;&#x7edf;&#x4f1a;&#x8fdb;&#x884c;&#x5408;&#x5e76;&#xff08;minor&#x3001; major compaction&#xff09;&#xff0c;&#x5728;&#x5408;&#x5e76;&#x8fc7;&#x7a0b;&#x4e2d;&#x4f1a;&#x8fdb;&#x884c;&#x7248;&#x672c;&#x5408;&#x5e76;&#x548c;&#x5220;&#x9664;&#x5de5;&#x4f5c; &#xff08;majar&#xff09;&#xff0c;&#x5f62;&#x6210;&#x66f4;&#x5927;&#x7684;storefile &#x2013; &#x5f53;&#x4e00;&#x4e2a;region&#x6240;&#x6709;storefile&#x7684;&#x5927;&#x5c0f;&#x548c;&#x8d85;&#x8fc7;&#x4e00;&#x5b9a;&#x9608;&#x503c;&#x540e;&#xff0c;&#x4f1a;&#x628a;&#x5f53;&#x524d;&#x7684;region &#x5206;&#x5272;&#x4e3a;&#x4e24;&#x4e2a;&#xff0c;&#x5e76;&#x7531;hmaster&#x5206;&#x914d;&#x5230;&#x76f8;&#x5e94;&#x7684;regionserver&#x670d;&#x52a1;&#x5668;&#xff0c;&#x5b9e;&#x73b0;&#x8d1f;&#x8f7d; &#x5747;&#x8861; &#x2013; &#x5ba2;&#x6237;&#x7aef;&#x68c0;&#x7d22;&#x6570;&#x636e;&#xff0c;&#x5148;&#x5728;memstore&#x627e;&#xff0c;&#x627e;&#x4e0d;&#x5230;&#x518d;&#x627e;storefile">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+<node CREATED="1519962722231" ID="ID_1449874972" MODIFIED="1520214983884" POSITION="right" TEXT="&#x6743;&#x9650;&#x63a7;&#x5236;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node CREATED="1519962739535" ID="ID_938255614" MODIFIED="1520214983884" POSITION="right" TEXT="&#x5b58;&#x50a8;&#x4ee5;&#x53ca;&#x8c03;&#x4f18;">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
