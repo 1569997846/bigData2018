9457b870f0920faffd59ad136bb65ed2bef66551
liaohualue <m18775128300@163.com>
2018-03-22 16:41:33 +0800

    机器学习

---
+++ machine_learning/k-means算法.mm
@@ -0,0 +1,145 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1521601603182" ID="ID_920053500" MODIFIED="1521601622645" TEXT="k-means&#x7b97;&#x6cd5;">
+<node CREATED="1521601642996" ID="ID_955857000" MODIFIED="1521601687311" POSITION="right" TEXT="&#x5927;&#x7eb2;">
+<node CREATED="1521601699768" ID="ID_962234240" MODIFIED="1521601702893" TEXT="Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x539f;&#x7406;">
+<node CREATED="1521601705697" ID="ID_574385554" MODIFIED="1521601714716" TEXT="Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x6982;&#x8ff0;">
+<node CREATED="1521602085128" ID="ID_20398013" MODIFIED="1521602092708">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x6982;&#x8ff0;.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1521601722137" ID="ID_1287667580" MODIFIED="1521601733592" TEXT="Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x56fe;&#x793a; ">
+<node CREATED="1521602191840" ID="ID_889686510" MODIFIED="1521602218442">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x7b97;&#x6cd5;&#x56fe;&#x793a;001.png" />
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1521602221516" ID="ID_1436421328" MODIFIED="1521602226786">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x7b97;&#x6cd5;&#x56fe;&#x793a;002.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1521601741369" ID="ID_1877994270" MODIFIED="1521601742951" TEXT="Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x8981;&#x70b9;">
+<node CREATED="1521602330656" ID="ID_1236329430" MODIFIED="1521602336821" TEXT="&#x6838;&#x5fc3;&#x601d;&#x60f3;">
+<node CREATED="1521602339736" ID="ID_1839645449" MODIFIED="1521602364915">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x6838;&#x5fc3;&#x601d;&#x60f3;.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1521602379328" ID="ID_483044753" MODIFIED="1521602387575" TEXT="&#x7b97;&#x6cd5;&#x6b65;&#x9aa4;&#x56fe;&#x89e3;">
+<node CREATED="1521602417513" ID="ID_204305029" MODIFIED="1521602423380">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x7b97;&#x6cd5;&#x6b65;&#x9aa4;&#x56fe;&#x89e3;.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1521602446360" ID="ID_1516600722" MODIFIED="1521602454171" TEXT="&#x5b9e;&#x73b0;&#x7b97;&#x6cd5;&#x6b65;&#x9aa4;">
+<node CREATED="1521602504770" ID="ID_203596933" MODIFIED="1521602512810">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x5b9e;&#x73b0;&#x7b97;&#x6cd5;&#x7684;&#x6b65;&#x9aa4;.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1521601748625" ID="ID_832712535" MODIFIED="1521601759445" TEXT="Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x6848;&#x4f8b;">
+<node CREATED="1521601761664" ID="ID_1257167481" MODIFIED="1521601768717" TEXT="&#x9700;&#x6c42;"/>
+<node CREATED="1521601771472" ID="ID_1444162050" MODIFIED="1521601782262" TEXT="&#x7528;Numpy&#x624b;&#x52a8;&#x5b9e;&#x73b0;"/>
+<node CREATED="1521601789481" ID="ID_860108932" MODIFIED="1521601791112" TEXT="&#x7528;Scikili&#x673a;&#x5668;&#x5b66;&#x4e60;&#x7b97;&#x6cd5;&#x5e93;&#x5b9e;&#x73b0;"/>
+</node>
+<node CREATED="1521601801521" ID="ID_1811646421" MODIFIED="1521601803022" TEXT="Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x8865;&#x5145;">
+<node CREATED="1521601809336" ID="ID_144049855" MODIFIED="1521601810814" TEXT="&#x7b97;&#x6cd5;&#x7f3a;&#x70b9;">
+<node CREATED="1521602574441" ID="ID_503951239" MODIFIED="1521602581730">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x7b97;&#x6cd5;&#x7684;&#x7f3a;&#x70b9;.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1521602613897" ID="ID_1690632262" MODIFIED="1521602634091">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x7f3a;&#x70b9;002.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1521602652234" ID="ID_401256367" MODIFIED="1521602693402">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x7f3a;&#x70b9;003.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1521601812601" ID="ID_593955992" MODIFIED="1521601818369" TEXT="&#x6539;&#x826f;&#x601d;&#x8def;">
+<node CREATED="1521602750176" ID="ID_410593927" MODIFIED="1521602755753">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="&#x6539;&#x826f;&#x601d;&#x8def;.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1521601890505" ID="ID_1969620971" MODIFIED="1521601903119" POSITION="right" TEXT="&#x76ee;&#x6807;">
+<node CREATED="1521601905105" ID="ID_1587455554" MODIFIED="1521601915608" TEXT="1&#x3001;&#x7406;&#x89e3;Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x7684;&#x6838;&#x5fc3;&#x601d;&#x60f3;"/>
+<node CREATED="1521601917680" ID="ID_1703655793" MODIFIED="1521601928116" TEXT="2&#x3001;&#x7406;&#x89e3;Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x7684;&#x4ee3;&#x7801;&#x5b9e;&#x73b0;"/>
+<node CREATED="1521601939303" ID="ID_394407830" MODIFIED="1521601941735" TEXT="3&#x3001;&#x638c;&#x63e1;Kmeans&#x805a;&#x7c7b;&#x7b97;&#x6cd5;&#x7684;&#x5e94;&#x7528;&#x6b65;&#x9aa4;&#xff1a;&#x6570;&#x636e;&#x5904;&#x7406;&#x3001;&#x5efa;&#x6a21;&#x3001;&#x8fd0;&#x7b97;&#x548c;&#x7ed3;&#x679c;&#x5224;&#x5b9a;"/>
+</node>
+</node>
+</map>
